48650292c8a9edfac5eaad2788aaa54e9c5683f1
Vinícius Bohrer <vbsanttos@gmail.com>
2020-03-21 18:56:54 -0300

    General report model, table and controller added

---
+++ controllers/GeneralReportController.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const { GeneralReport } = require("../models");
+
+class GeneralReportController {
+  create = async data => {
+    const generalreport = await GeneralReport.create(data);
+    return generalreport.get();
+  };
+  index = async () => {
+    const generalreports = await GeneralReport.findAll();
+    const response = generalreports.map(generalreport => generalreport.get());
+    return response;
+  };
+  show = async id => {
+    const generalreport = await GeneralReport.findOne({
+      where: {
+        id
+      }
+    });
+    return generalreport.get();
+  };
+  update = async data => {
+    const generalreport = await GeneralReport.update(data, {
+      where: {
+        id: data.id
+      }
+    });
+    return generalreport;
+  };
+  delete = async id => {
+    const generalreport = await GeneralReport.destroy({
+      where: {
+        id
+      }
+    });
+    return generalreport;
+  };
+}
+
+module.exports = GeneralReportController;
---
+++ models/generalreport.js
@@ -0,0 +1,18 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const GeneralReport = sequelize.define(
+    "GeneralReport",
+    {
+      report: DataTypes.STRING,
+      condominioId: DataTypes.INTEGER
+    },
+    {}
+  );
+  GeneralReport.associate = function(models) {
+    // associations can be defined here
+    GeneralReport.belongsTo(models.Condominio, {
+      foreignKey: "condominioId"
+    });
+  };
+  return GeneralReport;
+};
