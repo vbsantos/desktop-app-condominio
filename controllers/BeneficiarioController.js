baf493ded65ae2f2633c68d218ed4d3079fec370
Vinícius Bohrer <vbsanttos@gmail.com>
2020-03-22 17:44:50 -0300

    Added reports to nested database calls

--- controllers/BeneficiarioController.js
+++ controllers/BeneficiarioController.js
@@ -35,29 +35,42 @@ class BeneficiarioController {
     let Despesas = [];
     let Valores = [];
     let Condominios = [];
+    let RelatorioGeral = [];
+    let RelatorioIndividual = [];
     beneficiario.Condominios.forEach(condominio => {
+      condominio.GeneralReports.forEach(generalReport => {
+        RelatorioGeral.push(generalReport.get());
+      });
       condominio.Despesas.forEach(despesa => {
         despesa.Valors.forEach(valor => {
           Valores.push(valor.get());
         });
         const temp3 = despesa.get();
-        // Valores individuais
         delete temp3.Valors;
         temp3.Valores = Valores;
         Despesas.push(temp3);
         Valores = [];
       });
       condominio.Pagantes.forEach(pagante => {
+        pagante.IndividualReports.forEach(individualReport => {
+          RelatorioIndividual.push(individualReport.get());
+        });
         pagante.Boletos.forEach(boleto => {
           Boletos.push(boleto.get());
         });
         const temp1 = pagante.get();
         temp1.Boletos = Boletos;
-        Pagantes.push(temp1);
         Boletos = [];
+        temp1.Relatorios = RelatorioIndividual;
+        RelatorioIndividual = [];
+        delete temp1.IndividualReports;
+        Pagantes.push(temp1);
       });
       const temp2 = condominio.get();
-      // delete temp2.Conta;
+      delete temp2.GeneralReports;
+      // Sort pra colocar "RelatoriosGeiras" por ordem inversa de criação (id)
+      temp2.Relatorios = RelatorioGeral.sort((a, b) => (a.id < b.id ? 1 : -1));
+      RelatorioGeral = [];
       // Sort pra colocar "contas" por ordem de criação (id)
       temp2.Despesas = Despesas.sort((a, b) => (a.id > b.id ? 1 : -1));
       Despesas = [];
