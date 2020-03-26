c4a61c8d885a0853a4b586fb9b1c0a01421c2a6c
Vinícius Bohrer <vbsanttos@gmail.com>
2020-03-26 12:23:52 -0300

    add precoAgua field to Valores

--- migrations/20200312221808-create-valor.js
+++ migrations/20200312221808-create-valor.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      precoAgua: {
+        type: Sequelize.STRING
+      },
       agua: {
         type: Sequelize.STRING
       },
