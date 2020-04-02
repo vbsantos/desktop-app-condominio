84d595f1e3b05b8275c2cc1739e35883dbabef3e
Vinícius Bohrer <vbsanttos@gmail.com>
2020-04-02 14:25:55 -0300

    refactor reports folder structure

---
+++ src/reports/relatorioGeral/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+// MATERIAL UI COMPONENTS
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell
+} from "@material-ui/core";
+
+// CSS
+import "./style.css";
+
+export default function RatearContas(props) {
+  return (
+    <div>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
