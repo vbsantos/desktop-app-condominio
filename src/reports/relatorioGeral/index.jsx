58c181e81278449227747ab7f19a6b79edccf41e
Vinícius Bohrer <vbsanttos@gmail.com>
2020-04-03 11:52:08 -0300

    add general report visualizer and fix css

--- src/reports/relatorioGeral/index.jsx
+++ src/reports/relatorioGeral/index.jsx
@@ -13,22 +13,94 @@ import {
 // CSS
 import "./style.css";
 
-export default function RatearContas(props) {
+export default function relatorioGeral(props) {
+  const { report } = props;
+
+  console.log("Report inside relatorioGeral:", report);
+
   return (
-    <div>
+    <div id="relatorioGeral">
       <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell></TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+        {report.map(categoria => {
+          // cada categoria
+          let subtotal = 0;
+          return (
+            categoria.table && (
+              <Table key={categoria.name + "table"}>
+                <TableHead>
+                  <TableRow key={categoria.name + "header"} className="Black">
+                    <TableCell className="col1">{categoria.name}</TableCell>
+                    <TableCell className="col2">Parcela</TableCell>
+                    <TableCell className="col3">Valor</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {categoria.data.map(despesa => {
+                    // cada conta dessa categoria
+                    subtotal += Number(despesa.valor);
+                    return (
+                      <TableRow key={categoria + despesa.id} className="Linha">
+                        <TableCell className="col1">{despesa.nome}</TableCell>
+                        <TableCell className="col2">
+                          {despesa.permanente
+                            ? "Fixa"
+                            : `${despesa.parcelaAtual +
+                                " de " +
+                                despesa.numParcelas}`}
+                        </TableCell>
+                        <TableCell className="col3">
+                          {"R$ " + Number(despesa.valor).toFixed(2)}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                  <TableRow key={categoria + "subtotal"} className="Black">
+                    <TableCell className="col1">SUB-TOTAL:</TableCell>
+                    <TableCell className="col2"></TableCell>
+                    <TableCell className="col3">
+                      {"R$ " + Number(subtotal).toFixed(2)}
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            )
+          );
+        })}
+
+        {/* fundo reserva e soma total dos valores */}
+        {report.map(info => {
+          if (!info.table) {
+            if (info.name === "fundoReserva") {
+              return (
+                <Table key={"fundoReserva"}>
+                  <TableHead>
+                    <TableRow className="Black" id="fundoReservaRow">
+                      <TableCell className="col1">Fundo Reserva</TableCell>
+                      <TableCell className="col2"></TableCell>
+                      <TableCell className="col3">
+                        {"R$ " + Number(info.data).toFixed(2)}
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                </Table>
+              );
+            } else if (info.name === "total") {
+              return (
+                <Table key={"total"}>
+                  <TableHead>
+                    <TableRow className="Black">
+                      <TableCell className="col1">TOTAL:</TableCell>
+                      <TableCell className="col2"></TableCell>
+                      <TableCell className="col3">
+                        {"R$ " + Number(info.data).toFixed(2)}
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                </Table>
+              );
+            }
+          }
+        })}
       </TableContainer>
     </div>
   );
--- src/reports/relatorioRegistrar/index.jsx
+++ src/reports/relatorioRegistrar/index.jsx
@@ -42,88 +42,92 @@ export default function RelatorioCondominioRegistrar(props) {
   };
 
   return (
-    <TableContainer ref={reportRef}>
-      {categorias.map(categoria => {
-        const categoriaContas = despesas.filter(
-          despesa => despesa.categoria === categoria
-        );
-        const subtotal = categoriaContas.reduce((acc, despesa) => {
-          return acc + Number(despesa.valor);
-        }, 0);
-        return (
-          <Table key={categoria + "table"}>
-            <TableHead>
-              <TableRow key={categoria + "header"} className="Black">
-                <TableCell className="col1">{categoria}</TableCell>
-                <TableCell className="col2">Parcela</TableCell>
-                <TableCell className="col3">Valor</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {categoriaContas.map(categoriaConta => (
-                <TableRow
-                  key={categoria + categoriaConta.id}
-                  className="Linha"
-                  onClick={() => selectAndOpenDialog(categoriaConta.id)}
-                >
-                  <TableCell className="col1">{categoriaConta.nome}</TableCell>
-                  <TableCell className="col2">
-                    {categoriaConta.permanente
-                      ? "Fixa"
-                      : `${categoriaConta.parcelaAtual +
-                          " de " +
-                          categoriaConta.numParcelas}`}
-                  </TableCell>
+    <div id="relatorioCondominioRegistrar">
+      <TableContainer ref={reportRef}>
+        {categorias.map(categoria => {
+          const categoriaContas = despesas.filter(
+            despesa => despesa.categoria === categoria
+          );
+          const subtotal = categoriaContas.reduce((acc, despesa) => {
+            return acc + Number(despesa.valor);
+          }, 0);
+          return (
+            <Table key={categoria + "table"}>
+              <TableHead>
+                <TableRow key={categoria + "header"} className="Black">
+                  <TableCell className="col1">{categoria}</TableCell>
+                  <TableCell className="col2">Parcela</TableCell>
+                  <TableCell className="col3">Valor</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {categoriaContas.map(categoriaConta => (
+                  <TableRow
+                    key={categoria + categoriaConta.id}
+                    className="Linha"
+                    onClick={() => selectAndOpenDialog(categoriaConta.id)}
+                  >
+                    <TableCell className="col1">
+                      {categoriaConta.nome}
+                    </TableCell>
+                    <TableCell className="col2">
+                      {categoriaConta.permanente
+                        ? "Fixa"
+                        : `${categoriaConta.parcelaAtual +
+                            " de " +
+                            categoriaConta.numParcelas}`}
+                    </TableCell>
+                    <TableCell className="col3">
+                      {"R$ " + Number(categoriaConta.valor).toFixed(2)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow key={categoria + "subtotal"} className="Black">
+                  <TableCell className="col1">SUB-TOTAL:</TableCell>
+                  <TableCell className="col2"></TableCell>
                   <TableCell className="col3">
-                    {"R$ " + Number(categoriaConta.valor).toFixed(2)}
+                    {"R$ " + Number(subtotal).toFixed(2)}
                   </TableCell>
                 </TableRow>
-              ))}
-              <TableRow key={categoria + "subtotal"} className="Black">
-                <TableCell className="col1">SUB-TOTAL:</TableCell>
+              </TableBody>
+            </Table>
+          );
+        })}
+        {percentage[0] !== 0 && (
+          <Table key={"fundoReserva"}>
+            <TableHead>
+              <TableRow
+                className="Black"
+                id="fundoReservaRow"
+                onClick={() =>
+                  selectAndOpenDialog(
+                    despesas.filter(despesa => despesa.fundoReserva)[0].id || ""
+                  )
+                }
+              >
+                <TableCell className="col1">
+                  Fundo Reserva - {percentage[0]} %
+                </TableCell>
                 <TableCell className="col2"></TableCell>
                 <TableCell className="col3">
-                  {"R$ " + Number(subtotal).toFixed(2)}
+                  {"R$ " + percentage[1].toFixed(2)}
                 </TableCell>
               </TableRow>
-            </TableBody>
+            </TableHead>
           </Table>
-        );
-      })}
-      {percentage[0] !== 0 && (
-        <Table key={"fundoReserva"}>
+        )}
+        <Table key={"total"}>
           <TableHead>
-            <TableRow
-              className="Black"
-              id="fundoReservaRow"
-              onClick={() =>
-                selectAndOpenDialog(
-                  despesas.filter(despesa => despesa.fundoReserva)[0].id || ""
-                )
-              }
-            >
-              <TableCell className="col1">
-                Fundo Reserva - {percentage[0]} %
-              </TableCell>
+            <TableRow className="Black">
+              <TableCell className="col1">TOTAL:</TableCell>
               <TableCell className="col2"></TableCell>
               <TableCell className="col3">
-                {"R$ " + percentage[1].toFixed(2)}
+                {"R$ " + (total + percentage[1]).toFixed(2)}
               </TableCell>
             </TableRow>
           </TableHead>
         </Table>
-      )}
-      <Table key={"total"}>
-        <TableHead>
-          <TableRow className="Black">
-            <TableCell className="col1">TOTAL:</TableCell>
-            <TableCell className="col2"></TableCell>
-            <TableCell className="col3">
-              {"R$ " + (total + percentage[1]).toFixed(2)}
-            </TableCell>
-          </TableRow>
-        </TableHead>
-      </Table>
-    </TableContainer>
+      </TableContainer>
+    </div>
   );
 }
