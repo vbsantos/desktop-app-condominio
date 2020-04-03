8da09fdee42c9928957126b37b0d7224eec77612
Vinícius Bohrer <vbsanttos@gmail.com>
2020-04-03 16:52:44 -0300

    add individual report visualizer

--- src/reports/relatorioIndividual/index.jsx
+++ src/reports/relatorioIndividual/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // MATERIAL UI COMPONENTS
 import {
@@ -13,20 +13,61 @@ import {
 // CSS
 import "./style.css";
 
-export default function RatearContas(props) {
+export default function RelatorioIndividual(props) {
+  const { report } = props;
+  const reportData = props.report
+    .map(categoria =>
+      categoria.table
+        ? categoria.data.map(despesa => [despesa.nome, despesa.valor])
+        : [
+            [
+              categoria.name === "total" ? "Total" : "Fundo Reserva",
+              categoria.data
+            ]
+          ]
+    )
+    .flat(1);
+
+  const [tableSize, setTableSize] = useState(Math.ceil(reportData.length / 2));
+
+  const table1 = reportData.splice(0, tableSize);
+  const table2 = reportData;
+
+  console.warn(table1, table2);
+
   return (
-    <div>
+    <div id="relatorioIndividual">
       <TableContainer>
-        <Table>
+        <Table id="table1">
           <TableHead>
             <TableRow>
-              <TableCell></TableCell>
+              <TableCell id="nome">Despesa</TableCell>
+              <TableCell id="valor">{"Valor (R$) "}</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
+            {table1.map(despesa => (
+              <TableRow>
+                <TableCell id="nome">{despesa[0]}</TableCell>
+                <TableCell id="valor">{"R$ " + despesa[1]}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Table id="table2">
+          <TableHead>
             <TableRow>
-              <TableCell></TableCell>
+              <TableCell id="nome">Despesa</TableCell>
+              <TableCell id="valor">{"Valor (R$) "}</TableCell>
             </TableRow>
+          </TableHead>
+          <TableBody>
+            {table2.map(despesa => (
+              <TableRow>
+                <TableCell id="nome">{despesa[0]}</TableCell>
+                <TableCell id="valor">{"R$ " + despesa[1]}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
