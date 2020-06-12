d83b59962fdda8368d1fd96b4d4051a4193b3bcf
Vinícius Bohrer <vbsanttos@gmail.com>
2020-06-11 22:31:43 -0300

    add water report header

---
+++ src/components/headerWaterReport/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+// IMAGE
+import logo from "../../assets/icon.png";
+
+// CSS
+import "./style.css";
+
+export default function WaterReportHeader(props) {
+  const { nomeCondominio } = props;
+  const { enderecoCondominio } = props;
+  const { precoAgua } = props;
+  const { date } = props;
+
+  const validDate = typeof date !== "undefined";
+
+  return (
+    <div id="WaterReportHeader">
+      <img id="header-left" src={logo} alt="Logo" />
+      <div id="header-right">
+        <div id="header-line2">
+          <span className="bold">{nomeCondominio}</span>
+          {enderecoCondominio && (
+            <span className="bold">{" - " + enderecoCondominio}</span>
+          )}
+        </div>
+
+        {validDate ? (
+          <div className="bold" id="header-line1">
+            {`Leitura de Água ${date.mes} de ${date.ano}`}
+          </div>
+        ) : (
+          <div className="bold" id="header-line1">
+            {"Leitura de Água"}
+          </div>
+        )}
+
+        {enderecoCondominio && (
+          <div id="header-line3">
+            <span className="bold">{`Responsável: _______________ Data: ___ / ___ / ___ Valor do m³: R$ ${precoAgua}`}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
