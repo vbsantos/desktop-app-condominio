f40fd744c102c8d31a5585ad9146f4c4666ba476
Vinícius Bohrer <vbsanttos@gmail.com>
2020-08-16 18:56:37 -0300

    fix: generate reports w/o fundo reserva

--- src/reports/relatorioGeral/index.jsx
+++ src/reports/relatorioGeral/index.jsx
@@ -31,10 +31,12 @@ export default function RelatorioGeral(props) {
   const fundoReserva = report.find(
     (data) => !data.table && data.name.includes("Fundo Reserva")
   );
-  fundoReserva.data = isNaN(fundoReserva.data.value)
-    ? { value: fundoReserva.data }
-    : fundoReserva.data;
-  // console.warn("fundoReserva", fundoReserva);
+  if (fundoReserva) {
+    fundoReserva.data = isNaN(fundoReserva.data.value)
+      ? { value: fundoReserva.data }
+      : fundoReserva.data;
+    // console.warn("fundoReserva", fundoReserva);
+  }
 
   const total = report.find((data) => !data.table && data.name === "total");
   const informacoes = report.find(
