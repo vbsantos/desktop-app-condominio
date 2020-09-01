bfcfc88f3f5403d73d7de99835f503a7cdffc5ec
Vinícius Bohrer <vbsanttos@gmail.com>
2020-09-01 19:51:26 -0300

    fix report header

--- src/components/headerReserveFundReport/index.jsx
+++ src/components/headerReserveFundReport/index.jsx
@@ -11,13 +11,21 @@ export default function ReserveFundReportHeader(props) {
   const { enderecoCondominio } = props;
   const { date } = props;
 
+  const validDate = typeof date !== "undefined";
+
   return (
     <div id="ReserveFundReportHeader">
       <img id="header-left" src={logo} alt="Logo" />
       <div id="header-right">
-        <div className="bold" id="header-line1">
-          {`Extrato Fundo Reserva ${date.mes} de ${date.ano}`}
-        </div>
+        {validDate ? (
+          <div className="bold" id="header-line1">
+            {`Extrato Fundo Reserva ${date.mes} de ${date.ano}`}
+          </div>
+        ) : (
+          <div className="bold" id="header-line1">
+            {"Extrato Fundo Reserva"}
+          </div>
+        )}
         <div id="header-line2">
           <span className="bold">{nomeCondominio}</span>
         </div>
